1896d99a7fe4cba47928b47a6d20c8f20ba49047
googyanas <googyanas@gmail.com>
2015-04-09 19:38:56 +0100

    3.4.12

--- net/bluetooth/l2cap_core.c
+++ net/bluetooth/l2cap_core.c
@@ -1063,10 +1063,11 @@ static void l2cap_conn_ready(struct l2cap_conn *conn)
 {
 	struct l2cap_chan_list *l = &conn->chan_list;
 	struct sock *sk;
+	struct hci_conn *hcon = conn->hcon;
 
 	BT_DBG("conn %p", conn);
 
-	if (!conn->hcon->out && conn->hcon->type == LE_LINK)
+	if (!hcon->out && hcon->type == LE_LINK)
 		l2cap_le_conn_ready(conn);
 
 	read_lock(&l->lock);
@@ -1077,7 +1078,7 @@ static void l2cap_conn_ready(struct l2cap_conn *conn)
 
 			if (conn->hcon->type == LE_LINK) {
 				u8 sec_level = l2cap_pi(sk)->sec_level;
-				u8 pending_sec = conn->hcon->pending_sec_level;
+				u8 pending_sec = hcon->pending_sec_level;
 
 				if (pending_sec > sec_level)
 					sec_level = pending_sec;
@@ -1097,14 +1098,14 @@ static void l2cap_conn_ready(struct l2cap_conn *conn)
 
 			bh_unlock_sock(sk);
 		}
-	} else if (conn->hcon->type == LE_LINK) {
-		smp_conn_security(conn, BT_SECURITY_HIGH);
+	} else if (hcon->type == LE_LINK) {
+		smp_conn_security(hcon, BT_SECURITY_HIGH);
 	}
 
 	read_unlock(&l->lock);
 
-	if (conn->hcon->out && conn->hcon->type == LE_LINK)
-		l2cap_le_conn_ready(conn);
+	if (hcon->out && hcon->type == LE_LINK)
+		l2cap_le_conn_ready(hcon);
 }
 
 /* Notify sockets that we cannot guaranty reliability anymore */
